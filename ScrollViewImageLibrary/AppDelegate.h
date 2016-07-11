d826bd3ee4f85a31399cf200fbf7ac7816e1a777
jpila <j.pilapil@hotmail.com>
2016-07-11 13:29:25 -0400

    Initial Commit

---
+++ ScrollViewImageLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageLibrary
+//
+//  Created by JOSE PILAPIL on 2016-07-11.
+//  Copyright © 2016 JOSE PILAPIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
